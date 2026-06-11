1ccaf4fb330107b2ee66d4aede6d5d4595135403
SiemonFrancart <siemon.francart@student.ehb.be>
2026-06-11 23:12:14 +0200

    archief mpa done

--- archief.js
+++ archief.js
@@ -3,6 +3,8 @@ let mapLocation = document.querySelector("#mapLocation");
 let mapAddress = document.querySelector("#mapAddress");
 let mapDate = document.querySelector("#mapDate");
 let mapTitle = document.querySelector("#mapTitle");
+let themeColor = document.querySelector("#themeColor");
+let mapDescription = document.querySelector("#mapDescription");
 
 IA3.addEventListener("click", function () {
 	// change innertext
@@ -10,5 +12,8 @@ IA3.addEventListener("click", function () {
 	mapAddress.innerText = IA3.dataset.address;
 	mapDate.innerText = IA3.dataset.date;
 	mapTitle.innerText = IA3.dataset.title;
+	mapDescription.innerText = IA3.dataset.description;
+	mapDescription.style.paddingTop = "40px";
+	themeColor.setAttribute("fill", "#d1deb9");
 	//change css svg color to the corresponding theme color
 });
