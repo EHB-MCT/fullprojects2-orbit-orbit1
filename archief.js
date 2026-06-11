9ae5b4f22a4ddaa473f066c409fa71e446a01579
SiemonFrancart <siemon.francart@student.ehb.be>
2026-06-11 22:32:25 +0200

    added extra info to overlay

--- archief.js
+++ archief.js
@@ -1,10 +1,14 @@
 let IA3 = document.querySelector("#IA3");
 let mapLocation = document.querySelector("#mapLocation");
 let mapAddress = document.querySelector("#mapAddress");
+let mapDate = document.querySelector("#mapDate");
+let mapTitle = document.querySelector("#mapTitle");
 
 IA3.addEventListener("click", function () {
 	// change innertext
 	mapLocation.innerText = IA3.dataset.location;
 	mapAddress.innerText = IA3.dataset.address;
+	mapDate.innerText = IA3.dataset.date;
+	mapTitle.innerText = IA3.dataset.title;
 	//change css svg color to the corresponding theme color
 });
