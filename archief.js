6f43239cfd5ac9c86c009102fb65cfa8e808b881
SiemonFrancart <siemon.francart@student.ehb.be>
2026-06-15 13:55:21 +0200

    javascript achief done

--- archief.js
+++ archief.js
@@ -1,4 +1,3 @@
-let IA3 = document.querySelector("#IA3");
 let mapLocation = document.querySelector("#mapLocation");
 let mapAddress = document.querySelector("#mapAddress");
 let mapDate = document.querySelector("#mapDate");
@@ -6,14 +5,18 @@ let mapTitle = document.querySelector("#mapTitle");
 let themeColor = document.querySelector("#themeColor");
 let mapDescription = document.querySelector("#mapDescription");
 
-IA3.addEventListener("click", function () {
-	// change innertext
-	mapLocation.innerText = IA3.dataset.location;
-	mapAddress.innerText = IA3.dataset.address;
-	mapDate.innerText = IA3.dataset.date;
-	mapTitle.innerText = IA3.dataset.title;
-	mapDescription.innerText = IA3.dataset.description;
+function updateOverlay(element) {
+	mapLocation.innerText = element.dataset.location;
+	mapAddress.innerText = element.dataset.address;
+	mapDate.innerText = element.dataset.date;
+	mapTitle.innerText = element.dataset.title;
+	mapDescription.innerText = element.dataset.description;
 	mapDescription.style.paddingTop = "40px";
-	themeColor.setAttribute("fill", "#d1deb9");
-	//change css svg color to the corresponding theme color
+	themeColor.setAttribute("fill", element.dataset.color);
+}
+
+document.querySelectorAll(".clickableEvent").forEach(function (region) {
+	region.addEventListener("click", function () {
+		updateOverlay(region);
+	});
 });
